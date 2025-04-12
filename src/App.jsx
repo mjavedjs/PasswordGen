730841947722d0ac4606be2696053a60aedc9bc6
mjavedjs <mjavedjs2006@gmail.com>
2025-04-11 23:41:39 -0700

    copyPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect } from "react";
-import { useState } from "react"
+import { useCallback, useEffect, useRef} from "react";
+import { useState } from "react"  
 import { use } from "react"
 
 function App(){
@@ -7,6 +7,8 @@ function App(){
   const [num,setnum] = useState(false);
   const [char,setchar] = useState(false);
   const [password,setpassword]= useState('');
+
+  const passwordRef = useRef(null);
 
   const PasswordGen =  useCallback(()=>{
      let password = "";
@@ -16,7 +18,7 @@ function App(){
     
       for(let i=1; i<length; i++){
          let char = Math.floor(Math.random() * string.length + 1);
-          password += string.charAt(char).trim();
+          password += string.charAt(char);
           setpassword(password)
       }
   },[length,num,char,setpassword]);
@@ -27,13 +29,17 @@ function App(){
     PasswordGen()
   },[length,num,char,PasswordGen]);
 
-
+const copyPassword = () => {
+  passwordRef.current?.select();
+  passwordRef.current?.setSelectionRange(0, 10)
+   window.navigator.clipboard.writeText(password);
+}
 return(
   <>   
  <div className="w-full bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 rounded-2xl px-5 py-5 mt-20 max-w-2xl mx-auto shadow-lg">
-  <h1 className="text-center text-5xl font-bold text-white mb-10 tracking-tight">
+  <h1 className="text-center text-3xl font-bold text-white mb-10 tracking-tight">
     🔐 Password Generator
-  </h1>
+  </h1> 
 
   <div className="bg-white max-w-md mx-auto shadow-xl rounded-xl px-6 py-5 flex items-center gap-4">
     <input
@@ -42,8 +48,9 @@ return(
       className="flex-1 outline-none py-3 px-4 bg-gray-100 text-gray-800 rounded-lg text-lg font-medium tracking-wide"
       placeholder="Generated password"
       readOnly
+      ref={passwordRef}
     />
-    <button onClick={()=>{ }}
+    <button onClick={copyPassword}
       className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-2 rounded-lg transition-all shadow-sm hover:shadow-md active:scale-95">
       Copy
     </button>
@@ -59,7 +66,7 @@ return(
         min={8}
         max={100}
         value={length}
-        onChange={(e) => setlength(e.target.value)}
+        onChange={(e)=>{setlength(e.target.value)}}
         className="cursor-pointer w-44 accent-blue-500"
       />
       <span className="text-blue-400 font-bold text-lg">{length}</span>
@@ -93,5 +100,8 @@ return(
   </>
 )
 }
-
+ 
 export default App
+
+
+
